e98cbcd43e385d0e9cfdb679464a19f144355794
Artem Markov <markovartyom795@gmail.com>
2024-07-30 17:57:43 +0300

    Api routes

---
+++ app/api/content/route.ts
@@ -0,0 +1,32 @@
+const posts = [
+	{
+		title: 'Lorem Ipsum',
+		slug: 'lorem-ipsum',
+		content:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.',
+	},
+	{
+		title: 'Lorem Ipsum',
+		slug: 'lorem-ipsum',
+		content:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.',
+	},
+	{
+		title: 'Lorem Ipsum',
+		slug: 'lorem-ipsum',
+		content:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.',
+	},
+	{
+		title: 'Lorem Ipsum',
+		slug: 'lorem-ipsum',
+		content:
+			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero.',
+	},
+];
+
+import {NextResponse} from 'next/server';
+
+export function GET() {
+	return NextResponse.json(posts);
+}
